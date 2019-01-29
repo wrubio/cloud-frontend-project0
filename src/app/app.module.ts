aa3fc2d5a2b94f9b37ce7f3b5990ddc58eb287ca
Wilmer A. Rubio Rojas <rubiorojasw@gmail.com>
2019-01-29 13:14:16 -0500

    Se crearon los componentes login, dashboard, page404, navbar y sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { Page404Component } from './pages/page404/page404.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    Page404Component,
+    DashboardComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
